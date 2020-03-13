9a3ab04fefbb416895b896c0c1b08ba9f58e4f75
samarvir <samarvir1996@gmail.com>
2020-03-13 19:45:37 +0530

    Add: Dropdown with axios

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
+import { Dropdown } from "semantic-ui-react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [country, setCountry] = useState("");
+  const [countryOptions, setcountryOptions] = useState([]);
 
   const handleInput = e => {
-    setCountry(e.target.value);
+    if (e.target.value !== "") {
+      axios
+        .post("http://localhost:5555/api/search/", {
+          query: e.target.value,
+        })
+        .then(res => {
+          console.log(res);
+          let countryWithFLag = res.data.map(country => {
+            country.image = {
+              src: `data:image/jpeg;base64,${country.flag_png}`,
+            };
+            return country;
+          });
+          setcountryOptions(countryWithFLag);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   };
 
   return (
     <div className="App">
       <h2>Autocomplete</h2>
       <p>Start typing:</p>
-      <form className="formAuto" autocomplete="off">
-        <div className="autocomplete">
-          <input
-            type="text"
-            name="Country"
-            value={country}
-            placeholder="Country"
-            onChange={handleInput}
-          />
-        </div>
-
-        <input type="submit" />
-      </form>
+      <div className="formAuto">
+        <Dropdown
+          placeholder="Select Country"
+          fluid
+          search
+          selection
+          onSearchChange={handleInput}
+          options={countryOptions}
+        ></Dropdown>
+      </div>
     </div>
   );
 }
